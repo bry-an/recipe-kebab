45a32d7e16205e6539523351c2e465c82d333223
Bryan Yunis <bryany@booj.com>
2021-05-02 01:13:12 -0600

    inital components with recipe api request

---
+++ src/components/RecipeCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let recipe = {};
+	const firstTwoIngredients = (ingredients) => ingredients.split(',').slice(0, 2);
+</script>
+
+<a href={recipe.href} target="_blank">
+	<h3>{recipe.title}</h3>
+</a>
+{#if recipe.thumbnail}
+	<img href="recipe.href" src={recipe.thumbnail} alt={`${recipe.thumbnail}-image`} />
+{:else}
+	<p>No image available, but I bet it looks great</p>
+{/if}
+
+<p>
+	This recipe combines the flavor of {firstTwoIngredients(recipe.ingredients)[0]} with the delightful
+	taste of {firstTwoIngredients(recipe.ingredients)[1]}
+</p>
+<hr />
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,53 @@
+<script>
+	import RecipeCard from '../components/RecipeCard.svelte';
+	let typeSearch = '';
+	let validationError = false;
+	let recipes = [];
+
+	function getRecipes() {
+		if (!typeSearch.length) {
+			validationError = true;
+			return;
+		}
+		validationError = false;
+		fetch(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=${typeSearch}`)
+			.then((res) => res.json())
+			.then((data) => (recipes = data.results));
+	}
+</script>
+
+<section>
+	<h1 class="header">Welcome to Recipe Monster!</h1>
+
+	<label for="type-input" class="label">What Would You Like To Cook?</label>
+	<input bind:value={typeSearch} type="text" id="type-input" />
+	<button on:click={getRecipes}>Get Recipes</button>
+	{#if validationError}
+		<p class="error">You must enter a search term</p>
+	{/if}
+	<ul>
+		{#each recipes as recipe}
+			<li>
+				<RecipeCard bind:recipe />
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	ul {
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+	.header {
+		text-align: center;
+	}
+	.label {
+		display: block;
+	}
+	.error {
+		color: red;
+		margin-top: 0.5rem;
+	}
+</style>
